63daec4669eb26e5de4828508d22b27f4f0cb5b0
beauhope <beauhope2021@gmail.com>
2026-02-16 17:28:49 +0000

    sw3

--- js/sw.js
+++ js/sw.js
@@ -1,20 +1,39 @@
-const CACHE_NAME = "mytodo-v2";
+const CACHE_NAME = "mytodo-v3";
 
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css",
   "./manifest.webmanifest",
+
+  "./css/style.css",
+
   "./js/app.js",
   "./js/auth.js",
-  "./js/firebase.js"
+  "./js/firebase.js",
+
+  "./fonts/Cairo-Regular.ttf",
+  "./fonts/Cairo-Bold.ttf"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .catch(err => console.error("Cache failed:", err))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
 });
 
